c8d55d8a488421ed936f11364a50cd06de9fba76
A3 Java <raphaclementej0b@gmail.com>
2026-05-18 19:15:29 -0300

    Add files via upload

---
+++ A3-main/A3/src/main/java/com/projeto/a3/A3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.projeto.a3;
+
+/**
+ *
+ * @author felip
+ */
+public class A3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
