093fe16824e05f527758d135180ef489a8835b0a
Artur Fornagieri <fornagieri.artur@gmail.com>
2026-06-06 11:51:15 -0300

    Add exception to DAO class

---
+++ src/main/java/com/usjt/projeto_a3/exception/DAOException.java
@@ -0,0 +1,11 @@
+package com.usjt.projeto_a3.exception;
+
+public class DAOException extends RuntimeException {
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
